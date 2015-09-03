869a7bc6afcd067572c37602c893795cd90b4edb
sipek18 <sipek18@up.edu>
2015-09-02 17:47:12 -0700

    Initial commit

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,9 +15,12 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // prime number tester
+        if (n==1) return false;
+        if (n==2) return true;
         if (n%2==0) return false;
 
-        for(int i=3;i*i<=n;i+=2) {
+
+        for(int i=3;i*i<=n;i++) {
             if (n % i == 0)
                 return false;
         }
